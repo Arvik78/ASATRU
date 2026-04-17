b215acaeddb397a40823b6b61a39a1882595aaa9
Arvik78 <arvikmahl@gmail.com>
2026-04-16 18:54:06 -0700

    Add seedsigner-psbt.js for SeedSigner PSBT integration

---
+++ seedsigner-psbt.js
@@ -0,0 +1,29 @@
+// seedsigner-psbt.js
+
+/**
+ * Create a Partially Signed Bitcoin Transaction (PSBT) for SeedSigner.
+ */
+function createPSBTForSeedSigner(inputs, outputs) {
+    // Implement PSBT creation logic here
+}
+
+/**
+ * Show PSBT QR code for SeedSigner.
+ */
+function showPSBTQR(psbt) {
+    // Implement QR code generation logic here
+}
+
+/**
+ * Scan the signed PSBT using a webcam.
+ */
+function scanSignedPSBT() {
+    // Implement webcam scanning logic here
+}
+
+/**
+ * Sign the transaction with SeedSigner.
+ */
+function signWithSeedSigner(signedData) {
+    // Implement transaction broadcasting logic here
+}
